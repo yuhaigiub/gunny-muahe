5be2585bc17a76ca1d2b68120c114ff370ffdf61
Yuhaigiub <buigiahuynt@gmail.com>
2023-07-18 15:33:45 +0700

    embed iframe to popup

--- src/gunny/float_left/float_left.js
+++ src/gunny/float_left/float_left.js
@@ -1,14 +1,3 @@
-import scrollFrame from "../../setup/js/_scrollFrame.2";
-
 $(document).on("gunny_float_left rendered", {}, function (event, uniqueId) {
 	const $this = $("#" + uniqueId);
-	$this.find(".item1").on("click", function (event) {
-		scrollFrame.func.scrollTo("#gunny_frame1_EwBx8", 0);
-	});
-	$this.find(".item5").on("click", function (event) {
-		scrollFrame.func.scrollTo("#gunny_frame2_ASesx", 10);
-	});
-	$this.find(".item6").on("click", function (event) {
-		scrollFrame.func.scrollTo("#gunny_frame3_Ajs24", 10);
-	});
 });
--- src/gunny/popup/popup.js
+++ src/gunny/popup/popup.js
@@ -1,3 +1,5 @@
+import scrollFrame from "../../setup/js/_scrollFrame.2";
+
 window.addEventListener("load", function (event) {
 	$("#popup-signin .signin-btn").on("click", function (event) {
 		$("#popup-signin").removeClass("active");
@@ -5,7 +7,18 @@ window.addEventListener("load", function (event) {
 		$(".gunny_frame3 .signin-btn").html("Đăng Xuất");
 	});
 
-	$("#popup-congrats .btn").on('click', function (event) {
+	$("#popup-congrats .btn").on("click", function (event) {
 		$("#popup-congrats").removeClass("active");
-	})
+	});
+
+
+	$(".item1").on("click", function (event) {
+		scrollFrame.func.scrollTo("#gunny_frame1_EwBx8", 10);
+	});
+	$(".item5").on("click", function (event) {
+		scrollFrame.func.scrollTo("#gunny_frame2_ASesx", 10);
+	});
+	$(".item6").on("click", function (event) {
+		scrollFrame.func.scrollTo("#gunny_frame3_Ajs24", 10);
+	});
 });
